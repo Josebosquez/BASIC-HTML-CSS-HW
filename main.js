721532802196e1ca7e41044928d26d8b29957b11
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-10-23 16:19:19 -0600

    added buttons to capture minus and add func. Count changes when buttons are pressed

---
+++ main.js
@@ -0,0 +1,23 @@
+const minus = document.querySelector('#minus')
+minus.style.backgroundColor = 'red';
+const add = document.querySelector('#add')
+add.style.backgroundColor = 'green';
+
+const counterCount = document.getElementById("counter")
+
+counter = 0
+console.log(counter)
+
+function addOne (){
+    counter = counter + 1
+    console.log(counter)
+    counterChange()
+}
+function minusOne (){
+    counter = counter - 1 
+    counterChange()
+}
+
+function counterChange(){
+    counterCount.innerText = `Count: ${counter}`
+}
