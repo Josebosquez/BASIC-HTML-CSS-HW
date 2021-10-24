9628edf7aa087418ff9ebb1191d2b8ec9e8fb355
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-10-24 12:04:46 -0600

    i was struggling with the loop, so i removed the functions from html and added event listeners.

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
-const minus = document.querySelector('#minus')
-minus.style.backgroundColor = 'red';
-const add = document.querySelector('#add')
-add.style.backgroundColor = 'green';
-
-
 const counterCount = document.getElementById("counter")
+const minus = document.querySelector('#minus')
+const add = document.querySelector('#add')
+
+minus.style.backgroundColor = 'red';
+add.style.backgroundColor = 'green';
+add.addEventListener('click', addOne)
+minus.addEventListener('click', minusOne)
 
 counter = 0
-console.log(counter)
 
-function addOne (){
-    counter = counter + 1
-    console.log(counter)
-    counterChange()
+function addOne(){
+    if (counter >=0){
+        counter = counter + 1
+        counterChange()
+        imgComponentAdd()
+    } else {
+        counter = counter +1 
+        counterChange()
+    }
 }
-function minusOne (){
-    counter = counter - 1 
-    counterChange()
+
+function minusOne(){
+    if (counter > 0){
+        counter = counter - 1 
+        counterChange()
+        imgComponentDelete()
+    } else {
+        counter = counter - 1 
+        counterChange()
+    }
 }
 
 function counterChange(){
     counterCount.innerText = `Count: ${counter}`
+}
+
+function imgComponentAdd(){
+    const imageCount = document.querySelector('#imageCount')
+    const img = document.createElement('div')
+    img.innerHTML = '<div><img src="https://img.icons8.com/office/30/000000/add-ticket.png"/></div>'
+    imageCount.appendChild(img)
+}
+
+function imgComponentDelete(){
+    if (counter>=0){
+        const imageCount = document.querySelector('#imageCount')
+        const img = document.createElement('div')
+        imageCount.removeChild(imageCount.lastElementChild)
+    }
 }
